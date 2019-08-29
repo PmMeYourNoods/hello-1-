d5a8b097cdd0d4d32472817a0439af8e4c39d009
Matthew Mai <46793725+PmMeYourNoods@users.noreply.github.com>
2019-08-29 14:30:20 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,14 +2,16 @@
 
 Name: Matthew Mai
 Date: 08/29/2019
-Objective:
+Objective: Create a simple Hello world style program
 
 */
+
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 	
 	printf("Hello Matthew.\n");
-	printf("Major: Business-Finance);
+	printf("Major: Business-Finance\n");
 	
 	return 0;
 }
